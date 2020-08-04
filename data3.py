39b2c882386b6b94e6563ac490380dd7d631ca1b
Ryan Lee <ryanlee619@gmail.com>
2020-08-04 16:39:57 -0700

    Initial Commit

---
+++ data3.py
@@ -0,0 +1,20 @@
+# February 12, 2020 - March 23, 2020
+
+# websites for potential sources:
+# certain twitter account tweets, replies, and retweets
+# ?also maybe random twitter accounts
+# marketwatch.com
+# finance.yahoo.com
+# reuters.com
+# bloomberg.com
+# fool.com
+# businessinsider.com
+# subreddits:reddit.com/r/wallstreetbets/, https://www.reddit.com/r/investing/,
+# finviz.com
+# seekingalpha.com
+# european: marketscreener.com/,
+# https://www.teletrader.com
+# facebook.com
+#https://www.fxstreet.com/analysis
+#http://www.google.com/finance
+# 
