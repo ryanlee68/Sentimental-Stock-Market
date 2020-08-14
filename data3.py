a956d7e9ce0c7457a3d10986452dd5cbfe46b7ac
Ryan Lee <ryanlee619@gmail.com>
2020-08-14 10:56:38 -0700

    got rid of class, ne format creqated

--- data3.py
+++ data3.py
@@ -1,5 +1,11 @@
 # February 12, 2020 - March 23, 2020
 
+TOP 3 MOST VISITED NEWS/MEDIA SITES:
+twitter
+facebook
+reddit
+
+
 # websites for potential sources:
 # certain twitter account tweets, replies, and retweets
 # ?also maybe random twitter accounts
@@ -23,35 +29,44 @@ import pandas as pd
 from selenium import webdriver
 import quandl
 
-class sectordf:
-    nasdaq_url = 'https://www.nasdaq.com/market-activity/stocks/screener?exchange=nyse&letter=0&render=download'
-    def __init__(self, website='universal', sector='universal'):
-        if website&sector=='universal':
-            pass
-        if website.lower()=='nasdaq':
-            if sector.lower()=='universal':
-                pass
-            if sector.lower()=='financial':
-                driver = webdriver.Chrome()
-                driver.get(nasdaq_url)
-            if sector.lower()=='healthcare':
-                pass
-            if sector.lower()=='conglomerates':
-                pass
-            if sector.lower()=='consumer goods':
-                pass
-            if sector.lower()=='services':
-                pass
-            if sector.lower()=='utilities':
-                pass
-            if sector.lower()=='basic materials':
-                pass
-            if sector.lower()=='technology':
-                pass
-            if sector.lower()=='industrial goods':
-                pass
+# class sectordf:
+#     nasdaq_url = 'https://www.nasdaq.com/market-activity/stocks/screener?exchange=nyse&letter=0&render=download'
+#     def __init__(self, website='universal', sector='universal'):
+#         if website&sector=='universal':
+#             pass
+#         if website.lower()=='nasdaq':
+#             if sector.lower()=='universal':
+#                 pass
+#             if sector.lower()=='financial':
+#                 driver = webdriver.Chrome()
+#                 driver.get(nasdaq_url)
+#             if sector.lower()=='healthcare':
+#                 pass
+#             if sector.lower()=='conglomerates':
+#                 pass
+#             if sector.lower()=='consumer goods':
+#                 pass
+#             if sector.lower()=='services':
+#                 pass
+#             if sector.lower()=='utilities':
+#                 pass
+#             if sector.lower()=='basic materials':
+#                 pass
+#             if sector.lower()=='technology':
+#                 pass
+#             if sector.lower()=='industrial goods':
+#                 pass
+#
+#     def nasdaq_get_sect():
+        # pass
 
-    def nasdaq_get_sect():
-        pass
+# df = sectordf(website='nasdaq', sector='financial')
 
-df = sectordf(website='nasdaq', sector='financial')
+marketwatch_url = 'https://www.marketwatch.com/latest-news?mod=top_nav'
+driver = webdriver.chrome()
+
+articles
+
+
+
+|
