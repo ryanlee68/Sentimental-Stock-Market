6eb1f066a61b37a166eb91cfeeeda4f40b6552f3
ryanl <ryanlee619@gmail.com>
2020-08-13 23:16:56 -0700

    more if statements

--- data3.py
+++ data3.py
@@ -22,3 +22,36 @@
 import pandas as pd
 from selenium import webdriver
 import quandl
+
+class sectordf:
+    nasdaq_url = 'https://www.nasdaq.com/market-activity/stocks/screener?exchange=nyse&letter=0&render=download'
+    def __init__(self, website='universal', sector='universal'):
+        if website&sector=='universal':
+            pass
+        if website.lower()=='nasdaq':
+            if sector.lower()=='universal':
+                pass
+            if sector.lower()=='financial':
+                driver = webdriver.Chrome()
+                driver.get(nasdaq_url)
+            if sector.lower()=='healthcare':
+                pass
+            if sector.lower()=='conglomerates':
+                pass
+            if sector.lower()=='consumer goods':
+                pass
+            if sector.lower()=='services':
+                pass
+            if sector.lower()=='utilities':
+                pass
+            if sector.lower()=='basic materials':
+                pass
+            if sector.lower()=='technology':
+                pass
+            if sector.lower()=='industrial goods':
+                pass
+
+    def nasdaq_get_sect():
+        pass
+
+df = sectordf(website='nasdaq', sector='financial')
